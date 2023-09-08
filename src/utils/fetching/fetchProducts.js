f2be71942f75bdaf8e825ca243616d38730ad66e
Abhijit Roy <abhiroy67981@gmail.com>
2023-09-08 13:34:19 +0530

    json added

--- src/utils/fetching/fetchProducts.js
+++ src/utils/fetching/fetchProducts.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getProducts = async () => {
   try {
     const prods = await axios(
-      "https://my-json-server.typicode.com/abhijitroy1997/react-ecommerce/Products"
+      "https://my-json-server.typicode.com/abhijitroy1997/react-ecommerce"
     );
     return prods;
   } catch (err) {
